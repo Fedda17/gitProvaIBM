fdf1eefd46aeeae63978e80a60fb7c28478f48e3
Fedda17 <fedda.martini@gmail.com>
2022-06-15 14:29:48 +0200

    Correction on main method

---
+++ src/src/Main.java
@@ -0,0 +1,10 @@
+import classi.Persona;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Persona p = new Persona("Federico", "Martini");
+        System.out.println(p);
+    }
+}
---
+++ src/src/classi/Persona.java
@@ -0,0 +1,36 @@
+package classi;
+
+public class Persona {
+    private String nome;
+    private String cognome;
+
+    public Persona(String nome, String cognome)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                '}';
+    }
+}
